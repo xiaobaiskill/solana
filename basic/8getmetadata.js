96f40d229ca991222ae14f03a20efadf260502a2
jmz <1125378902@qq.com>
2024-07-23 10:16:52 +0800

    get metadata

--- basic/8getmetadata.js
+++ basic/8getmetadata.js
@@ -7,6 +7,7 @@ const mint = new PublicKey("4n7AbgC6WWBMBqLa92vC3FmFQti6am17KS9jHEKgirPF")
 const asset = await fetchDigitalAsset(umi, mint)
 console.log(asset)
 
+console.log(asset.mint.decimals)
 console.log(asset.metadata.name)
 console.log(asset.metadata.symbol)
 console.log(asset.metadata.uri)
